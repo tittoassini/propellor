5b4ec51917ad7fada714ade46d0b0278c0565358
Pasqualino Titto Assini <tittoassini@gmail.com>
2014-05-10 23:12:49 +0100

    propellor spin

--- src/config.hs
+++ src/config.hs
@@ -29,15 +29,21 @@ hosts =
 	[ host "nano.quid2.org"
           & Apt.unattendedUpgrades
           -- & cabalUpdate
-          & quid2CheckService
 
+          & Ssh.randomHostKeys
+          
+          & quid2CheckService -- need to be accepted
+
+          & deployMyPackage "quid2-titto"
+          
         ,host "[quid2.org]:2222" & sshPubKey "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCR89KzcSBEJQ38/1gKIt/sqa4L71RzwoPS24qKyv5SmSJuWMpbPpoGIep6ucUYXFAtaLKwHxVXHfWrE4szZtYP+qVb9sVdPhhQ1GQThJFBHKJzSkk7jmO3tZ0gwl25GYebvTWoj+MszpdBxtofhHqiYmPFTSN/wlVGU1UmpZI6uUAUu+DA+1/uOHFCwCniQoLloiVDOGudKUAwaTubGc/qjVxQIfOACbbDN7CkbVA8NuKwqbfEZta3jafwk3HgIyQmDBU7gMYLWS0Z5GX4HsNEsogMsxNslNrG+EWwOgs1myVF2Uplw5h+1gnErREocWDrQ6jMAJRNp5QT4qO0bouX" 
 	]
 
-quid2CheckService = (Cron.job "quid2-check" "*/15 * * * *" "root" "/root" "/root/.cabal/bin/quid2-check check"
-                    `requires` Cron.job "quid2-check2" "00 23 * * *" "root" "/root" "/root/.cabal/bin/quid2-check check andReport"
-                    `requires` Cron.job "quid2-check3" "*/30 * * * *" "root" "/root" "/root/.cabal/bin/quid2-check /root/backup"
-                    )
+quid2CheckService = combineProperties "cronned quid2-check"
+                    [Cron.job "quid2-check" "*/15 * * * *" "root" "/root" "/root/.cabal/bin/quid2-check check"
+                    ,Cron.job "quid2-check2" "00 23 * * *" "root" "/root" "/root/.cabal/bin/quid2-check check andReport"
+                    ,Cron.job "quid2-check3" "*/30 * * * *" "root" "/root" "/root/.cabal/bin/quid2-check /root/backup"
+                    ]
                     `requires` 
                     (deployMyPackage "quid2-check"
                      `requires` Apt.installed ["nmap","mailutils"]
