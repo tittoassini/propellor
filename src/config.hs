7b806f0a51b64c34c90cf9c2427dd68cd8afa33f
Pasqualino Titto Assini <tittoassini@gmail.com>
2014-05-10 22:55:06 +0100

    propellor spin

--- src/config.hs
+++ src/config.hs
@@ -30,36 +30,18 @@ hosts =
           & Apt.unattendedUpgrades
           -- & cabalUpdate
           & quid2CheckService
-          {-
-		& Apt.stdSourcesList Unstable
-		& Apt.unattendedUpgrades
-		& Apt.installed ["etckeeper"]
-		& Apt.installed ["ssh"]
-		& User.hasSomePassword "root"
-		& Network.ipv6to4
-		& File.dirExists "/var/www"
-		& Docker.docked hosts "webserver"
-		& Docker.garbageCollected `period` Daily
-		& Cron.runPropellor "30 * * * *"
 
-	-- A generic webserver in a Docker container.
-	, Docker.container "webserver" "joeyh/debian-unstable"
-		& Docker.publish "80:80"
-		& Docker.volume "/var/www:/var/www"
-		& Apt.serviceInstalledRunning "apache2"
-        -}
-	-- add more hosts here...
-	--, host "foo.example.com" = ...
         ,host "quid2.org:2222" & sshPubKey "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCR89KzcSBEJQ38/1gKIt/sqa4L71RzwoPS24qKyv5SmSJuWMpbPpoGIep6ucUYXFAtaLKwHxVXHfWrE4szZtYP+qVb9sVdPhhQ1GQThJFBHKJzSkk7jmO3tZ0gwl25GYebvTWoj+MszpdBxtofhHqiYmPFTSN/wlVGU1UmpZI6uUAUu+DA+1/uOHFCwCniQoLloiVDOGudKUAwaTubGc/qjVxQIfOACbbDN7CkbVA8NuKwqbfEZta3jafwk3HgIyQmDBU7gMYLWS0Z5GX4HsNEsogMsxNslNrG+EWwOgs1myVF2Uplw5h+1gnErREocWDrQ6jMAJRNp5QT4qO0bouX" 
 	]
 
-quid2CheckService = (Cron.job "quid2-check" "*/15 * * * *" "root" "/root" "/root/.cabal/bin/quid2-check check")
+quid2CheckService = (Cron.job "quid2-check" "*/15 * * * *" "root" "/root" "/root/.cabal/bin/quid2-check check"
+                    `requires` Cron.job "quid2-check2" "00 23 * * *" "root" "/root" "/root/.cabal/bin/quid2-check check andReport"
+                    `requires` Cron.job "quid2-check3" "*/30 * * * *" "root" "/root" "/root/.cabal/bin/quid2-check /root/backup"
+                    )
                     `requires` 
                     (deployMyPackage "quid2-check"
                      `requires` Apt.installed ["nmap","mailutils"]
                      `requires` Ssh.knownHost hosts "quid2.org:2222" "root"
-                     -- 15 21 * * * /Users/titto/Library/Haskell/bin/quid2-check check andReport
-                     -- */30 * * * * /Users/titto/Library/Haskell/bin/quid2-check /root/backup
                      `requires` quidUtilPkg)             
              
 quidUtilPkg = deployMyPackage "quid2-util"
